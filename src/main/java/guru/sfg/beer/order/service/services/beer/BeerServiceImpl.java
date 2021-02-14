b38e40ffc88ba468fda2a6c9dd5a882a30ef136f
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2021-02-14 19:47:55 +0300

    Add last changes

--- src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -1,7 +1,6 @@
 package guru.sfg.beer.order.service.services.beer;
 
-import guru.sfg.beer.order.service.web.model.BeerDto;
-
+import guru.sfg.brewery.model.BeerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -14,8 +13,8 @@ import java.util.UUID;
 @Service
 public class BeerServiceImpl implements BeerService {
 
-    public final String BEER_PATH_V1 = "/api/v1/beer/";
-    public final String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/";
+    public static final String BEER_PATH_V1 = "/api/v1/beer/";
+    public static final String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/";
     private final RestTemplate restTemplate;
 
     private String beerServiceHost;
